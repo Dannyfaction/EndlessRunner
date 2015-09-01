a7705a2c86e2feed226cdae6dbad69a8187ac8ea
SkaterDanny <dannykruiswijk@hotmail.com>
2015-09-01 15:04:45 +0100

    Challange #1

---
+++ Challenge/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour 
+{
+	public GUISkin GameSkin;
+	private bool _isFirstMenu = true;
+	private string _gameName = "Ghostly Colors";
+	public Camera gameCamera;
+	
+	void OnGUI()
+	{
+		GUI.skin = GameSkin;
+		GUI.Label(new Rect(30, 30, 300, 25), _gameName, "Menu Title");
+		QualitySettings.SetQualityLevel(100, true);
+		FirstMenu();
+	}
+	
+	public void FirstMenu()
+	{
+		if (_isFirstMenu)
+		{
+			if (GUI.Button(new Rect(10, Screen.height / 2 - 30, 150, 25), "Start Game", "Button Style"))
+			{
+				Application.LoadLevel("Level01");
+			}
+			
+			if (GUI.Button(new Rect(10, Screen.height / 2, 150, 25), "Quit Game", "Button Style"))
+			{
+				Application.Quit();
+			}
+		}
+	}
+}
---
+++ Challenge/Assets/Scripts/TestKey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestKey: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape"))
+			Application.LoadLevel ("MainMenu");
+	}
+}
